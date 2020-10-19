052e326bf6d789f25f29cf074081a1950ddd0517
hagisan954 <kazumasoccer954@gmail.com>
2020-10-19 12:00:40 +0900

    てすと

---
+++ Miraisouzouten2292/app/src/main/java/jp/ac/hal/miraisouzouten2292/MapActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.hal.miraisouzouten2292;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
---
+++ Miraisouzouten2292/app/src/main/java/jp/ac/hal/miraisouzouten2292/ui/home/HomeViewModel.java
@@ -0,0 +1,22 @@
+package jp.ac.hal.miraisouzouten2292.ui.home;
+
+import android.widget.ImageView;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
